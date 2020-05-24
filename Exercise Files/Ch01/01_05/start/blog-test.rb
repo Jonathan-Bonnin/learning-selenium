2035c1dc99f8538df08788355061409e4180a021
Jonathan Bonnin <bonninj@gmail.com>
2020-05-25 00:35:43 +0200

    Chapter 1 of course completed

--- Exercise Files/Ch01/01_05/start/blog-test.rb
+++ Exercise Files/Ch01/01_05/start/blog-test.rb
@@ -1,23 +1,33 @@
 require "selenium-webdriver"
+require "rspec"
 
 # TEST: sign up for blog
-driver = Selenium::WebDriver.for :firefox
-# Go to signup form
-driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
+describe "Blog application" do
+    describe "sign up to the blog app" do
+        it "confirm that user can sign up" do
+            driver = Selenium::WebDriver.for :firefox
+            # Go to signup form
+            driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
 
-# Fill out and submit form
-username_field = driver.find_element(id: 'user_username')
-username_field.send_keys("user")
+            # Fill out and submit form
+            username_field = driver.find_element(id: 'user_username')
+            username_field.send_keys("user")
 
-email_field = driver.find_element(id: 'user_email')
-email_field.send_keys("user@test.com")
+            email_field = driver.find_element(id: 'user_email')
+            email_field.send_keys("user@test.com")
 
-password_field = driver.find_element(id: 'user_password')
-password_field.send_keys("password")
+            password_field = driver.find_element(id: 'user_password')
+            password_field.send_keys("password")
 
-sign_up_button = driver.find_element(id: 'submit')
-sign_up_button.click
+            sign_up_button = driver.find_element(id: 'submit')
+            sign_up_button.click
 
-# Confirm user is signed up successfully
+            # Confirm user is signed up successfully
+            banner = driver.find_element(id: "flash_success")
+            banner_text = banner.text
+            expect(banner_text).to eq("Welcome to the alpha blog user")
 
-driver.quit
+            driver.quit
+        end
+    end
+end
